30faa6a38b9f0a73f0d3b58030fe42bdbc1c89fd
Cozmaciuc Mircea <mircea.cozmaciuc@3pillarglobal.com>
2019-07-22 15:38:30 +0300

    Adaugat chesturi si sabii

--- src/com/company/Harta.java
+++ src/com/company/Harta.java
@@ -1,23 +1,31 @@
 package src.com.company;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Harta {
-    public int[][] a= new int[10][10];
+    public String[][] a= new String[10][10];
     public void Mapare()
     {
-        for(int i=0;i<9;i++)
-            for(int j=0;j<9;j++) {
-                a[i][j]=0;
+        for(int i=0;i<=9;i++)
+            for(int j=0;j<=9;j++) {
+                a[i][j]="-";
             }
-            a[5][8]=2;
-            a[4][3]=1;
-            a[2][7]=1;
+            a[5][8]="c";
+            a[4][3]="v";
+            a[2][7]="v";
+            a[9][5]="p";
     }
     public void Afisare()
     {
-        for(int i=0;i<9;i++)
+        for(int i=0;i<=9;i++)
         System.out.println(Arrays.toString(a[i]));
+        System.out.println("In fata ta este un chest. Scrie open pentru a-l deschide");
+        Scanner s = new Scanner(System.in);
+        String comanda=s.nextLine();
+        if(comanda.compareTo("sus")==0){
+            ///////////////////////////////////////
+        }
 
     }
 }
